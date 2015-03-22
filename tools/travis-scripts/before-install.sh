6c9a4ec035bec6ca5df245f4a5fa7a2f0de1d3bb
Sung Bae <laborercode@naver.com>
2015-03-22 18:33:26 +0900

    remove linux build

--- tools/travis-scripts/before-install.sh
+++ tools/travis-scripts/before-install.sh
@@ -54,30 +54,18 @@ wget  http://www.cocos2d-x.org/filedown/cocos2d-x-3.4.zip
 echo "Unzip cocos2d-x"
 unzip -o -q cocos2d-x-3.4.zip
 
-mv cocos2d-x-3.4 cocos
-cd $HOME/cocos/build
-
 echo "Install deps..."
 install_deps
-
-mkdir linux-build
-cd linux-build
-echo "Run cmake..."
-cmake ../..
-
-echo "Make cocos2d-x"
-make
 
 cd $HOME
 echo "Download android-ndk-r9d-linux-x86_64.tar.bz2..."
 wget http://dl.google.com/android/ndk/android-ndk-r9d-linux-x86_64.tar.bz2
-echo "Decompress android-ndk-r9d-linux-x86_64.tar.bzx2..."
+echo "Decompress android-ndk-r9d-linux-x86_64.tar.bz2..."
 tar xjf android-ndk-r9d-linux-x86_64.tar.bz2
 mv android-ndk-r9d android-ndk
 
 echo "Setup path"
 cd $HOME/cocos
 /usr/bin/env python ./setup.py
-
 
 exit $?
