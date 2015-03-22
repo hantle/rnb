6af1fbafe494361949e1d7c5d3c97db2ff39774b
Sung Bae <laborercode@naver.com>
2015-03-22 14:34:43 +0900

    add install_deps function

--- tools/travis-scripts/before-install.sh
+++ tools/travis-scripts/before-install.sh
@@ -1,3 +1,52 @@
+
+install_deps()
+{
+#install g++-4.9
+    sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+    sudo apt-get update
+
+#try to remove glfw2
+    sudo apt-get remove libglfw2
+
+    DEPENDS='libx11-dev'
+    DEPENDS+=' libxmu-dev'
+    DEPENDS+=' libglu1-mesa-dev'
+    DEPENDS+=' libgl2ps-dev'
+    DEPENDS+=' libxi-dev'
+    DEPENDS+=' g++-4.9'
+    DEPENDS+=' libzip-dev'
+    DEPENDS+=' libpng12-dev'
+    DEPENDS+=' libcurl4-gnutls-dev'
+    DEPENDS+=' libfontconfig1-dev'
+    DEPENDS+=' libsqlite3-dev'
+    DEPENDS+=' libglew-dev'
+    DEPENDS+=' libssl-dev'
+
+    MISSING=
+    echo "Checking for missing packages ..."
+    for i in $DEPENDS; do
+        if ! dpkg-query -W --showformat='${Status}\n' $i | grep "install ok installed" > /dev/null; then
+            MISSING+="$i "
+        fi
+    done
+
+    if [ -f /usr/bin/g++ ];then
+    sudo rm /usr/bin/g++
+    echo "remove old g++"
+    fi
+    sudo ln -s /usr/bin/g++-4.9 /usr/bin/g++
+
+    if [ -n "$MISSING" ]; then
+        TXTCOLOR_DEFAULT="\033[0;m"
+        TXTCOLOR_GREEN="\033[0;32m"
+        echo -e $TXTCOLOR_GREEN"Missing packages: $MISSING.\nYou may be asked for your password for package installation."$TXTCOLOR_DEFAULT
+        sudo apt-get --force-yes --yes install $MISSING
+    fi
+
+# install glfw
+    ../tools/travis-scripts/install_glfw.sh
+}
+
 cd $HOME
 echo "Download cocos2d-x-3.4..."
 wget  http://www.cocos2d-x.org/filedown/cocos2d-x-3.4.zip
@@ -8,7 +57,7 @@ mv cocos2d-x-3.4 cocos
 cd $HOME/cocos/build
 
 echo "Install deps..."
-sudo ./install-deps-linux.sh
+install_deps
 
 mkdir linux-build
 cd linux-build
