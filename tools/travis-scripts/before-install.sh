aed5afed9ffb3358606fd725ad00644b0bad827b
Sung Bae <laborercode@naver.com>
2015-03-22 13:56:36 +0900

    add more script and change travis script extension

--- tools/travis-scripts/before-install.sh
+++ tools/travis-scripts/before-install.sh
@@ -7,12 +7,24 @@ unzip -o -q cocos2d-x-3.4.zip
 mv cocos2d-x-3.4 cocos
 cd $HOME/cocos/build
 
-echo "install deps..."
+echo "Install deps..."
 sudo ./install-deps-linux.sh
 
 mkdir linux-build
 cd linux-build
 cmake ../..
 
-echo "make cocos2d-x"
+echo "Make cocos2d-x"
 make
+
+echo "Setup path"
+cd $HOME/cocos
+/usr/bin/env python ./setup.py
+
+cd $HOME
+echo "Download android-ndk-r9d-linux-x86_64.tar.bz2..."
+wget http://dl.google.com/android/ndk/android-ndk-r9d-linux-x86_64.tar.bz2
+echo "Decompress android-ndk-r9d-linux-x86_64.tar.bzx2..."
+tar xjf android-ndk-r9d-linux-x86_64.tar.bz2
+mv android-ndk-r9d android-ndk
+
