53bfa4add0b3bf5458f3769b366ab04d1ca82ac6
Administrator <admin@local.host>
2015-05-25 18:26:30 +0900

    Tiled Example Code

--- Classes/TileMap.cpp
+++ Classes/TileMap.cpp
@@ -29,6 +29,33 @@ TileMap::TileMap(Layer *layer)
      0 0 0 0 0 0 0 0 0
      */
     
+    //    http://www.cocos2d-x.org/wiki/TileMap
+//    // creating tmx node
+//    auto tilemap = TMXTiledMap::create("TileMap.tmx");
+//    tilemap->getLayer("background");
+//    this->addChild(tilemap);
+//    for (const auto& child : tilemap->getChildren()) {
+//        static_cast<SpriteBatchNode*>(child)->getTexture()->setAntiAliasTexParameters();
+//    }
+//    // get/add/delete/modify a tile
+//    auto tileLayer = tilemap->getLayer("background");
+//    auto tile = tileLayer->getTileAt(Vec2(1, 1));
+//    unsigned int gid = tileLayer->getTileGIDAt(Vec2(1, 1));
+//    tileLayer->setTileGID(gid, Vec2(3, 3));
+//    tileLayer->removeTileAt(Vec2(5, 5));
+//    // iterate a layer
+//    unsigned int newGid = 1;
+//    Size s = tileLayer->getLayerSize();
+//    for (int x = 0; x < s.width; ++x)
+//    {
+//        for (int y = 0; y < s.height; ++y)
+//        {
+//            tileLayer->setTileGID(newGid, Vec2(x, y));
+//        }
+//    }
+    
+    
+    
     for (int i = 0; i < 10; i++) {
         LayerColor *backgroundTile = LayerColor::create(Color4B::GRAY, kTILESIZE, kTILESIZE);
         LayerColor *frontTile = LayerColor::create(Color4B::BLUE, kTILESIZE-4, kTILESIZE-4);
